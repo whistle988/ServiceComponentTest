28f06db3810792fe5616e108afc95bae32b7cebe
vasileva_ea3 <vasileva_ea3@albank.ru>
2025-10-12 20:14:32 +0900

    Init commit

---
+++ app/src/main/java/com/example/servicecomponent/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.servicecomponent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.servicecomponent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this, 25))
+        }
+    }
+}
