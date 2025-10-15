953bff1a737d3dc7830f188775b12d9341dbd5fe
vasileva_ea3 <vasileva_ea3@albank.ru>
2025-10-15 16:22:31 +0900

    Add JobScheduler and IntentService2

---
+++ app/src/main/java/com/example/servicecomponent/MyIntentService2.kt
@@ -0,0 +1,45 @@
+package com.example.servicecomponent
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class MyIntentService2 : IntentService(NAME) {
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+        setIntentRedelivery(true)
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        log("onHandleIntent")
+        val page = intent?.getIntExtra(PAGE, 0) ?: 0
+        for (i in 0 until 5) {
+            Thread.sleep(1000)
+            log("Timer $i $page")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyForegroundService: $message")
+    }
+
+    companion object {
+
+        private const val NAME = "MyIntentService"
+        private const val PAGE = "page"
+
+        fun newIntent(context: Context, page: Int): Intent {
+            return Intent(context, MyIntentService2::class.java).apply {
+                putExtra(PAGE, page)
+            }
+        }
+    }
+}
